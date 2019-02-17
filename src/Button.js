1c42b4bb76b885b13f18c50b48469c77ba8c82b2
Adriano Mota <adrianowsh@hotmail.com>
2019-02-17 14:28:44 -0300

    initial

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+//Stateless component -- sem comportamento
+const Button = props => (
+  <button onClick={props.onClick}>{props.children}</button>
+);
+
+Button.defaultProps = {
+  children: "Salvar"
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.string
+};
+
+export default Button;
