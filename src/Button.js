f8cf44c1a4d7830bdbb99ecdb35cbe5965637777
Adriano Mota <adrianowsh@hotmail.com>
2019-02-17 14:49:05 -0300

    configurations

--- src/Button.js
+++ src/Button.js
@@ -1,18 +1,16 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-//Stateless component -- sem comportamento
-const Button = props => (
-  <button onClick={props.onClick}>{props.children}</button>
-);
+// Stateless component -- sem comportamento
+const Button = props => <button onClick={props.onClick}>{props.children}</button>;
 
 Button.defaultProps = {
-  children: "Salvar"
+  children: 'Salvar',
 };
 
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
-  children: PropTypes.string
+  children: PropTypes.string,
 };
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,51 +1,54 @@
-import React, { Component, Fragment } from "react";
-import { render } from "react-dom";
-import Button from "./Button";
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+import Button from './Button';
 
-import "./style.scss";
-//Statefull component -- com comportamento
+import './style.scss';
+// Statefull component -- com comportamento
 class App extends Component {
   state = {
-    counter: 0
+    counter: 0,
   };
 
-  //m[etodo executado imediatamente ap[os a inicializacao do componente
-  //ideal para chamadas de api ou carragar componentes de listagem ou cadastrar eventlisteners]
-  componentDidMount() {}
+  // m[etodo executado imediatamente ap[os a inicializacao do componente
+  // ideal para chamadas de api ou carragar componentes de listagem ou cadastrar eventlisteners]
+  // componentDidMount() {}
 
-  //ao contratio do didmount, idelal para fazer algo qdo o compoenente deixa de existir
-  componentWillMount() {}
+  // ao contratio do didmount, idelal para fazer algo qdo o compoenente deixa de existir
+  // componentWillMount() {}
 
-  //toda vez que o componente eh atualizado e o render é chamado passa antes por aqui
+  // toda vez que o componente eh atualizado e o render é chamado passa antes por aqui
   // retornando true ou false determinamos se renderizamos ou nao
   // é possível vetar a atuaizacao do render
-  //shouldComponentUpdate(nextProps, nextState) {}
+  // shouldComponentUpdate(nextProps, nextState) {}
 
-  //executa depois de sofrer atualizacap
-  componentDidUpdate(prevProps, presState) {}
+  // executa depois de sofrer atualizacap
+  // componentDidUpdate(prevProps, presState) {}
 
   handleClick = () => {
-    //this.state.counter += 1; -- nao mostra a mudanca de estado
-    this.setState({ counter: this.state.counter + 1 });
+    // this.state.counter += 1; -- nao mostra a mudanca de estado
+    this.setState({
+      counter: this.state.counter + 1,
+    });
 
-    //collocando funcao de callback para pegar o valor atuaizado imediatamente
+    // collocando funcao de callback para pegar o valor atuaizado imediatamente
     // this.setState({ counter: this.state.counter + 1 }, () => {
     //   console.log(this.state.counter);
     // });
 
-    //pegando o resultado diretamente da fila so state
-    //this.setState(state => ({ counter: state.counter + 1 }));
-    //this.setState(state => ({ counter: state.counter + 1 }));
+    // pegando o resultado diretamente da fila so state
+    // this.setState(state => ({ counter: state.counter + 1 }));
+    // this.setState(state => ({ counter: state.counter + 1 }));
   };
+
   render() {
     return (
       <Fragment>
         <h1 className="title">Hello React</h1>
-        <h2 style={{ color: "#f00" }}>{this.state.counter}</h2>
+        <h2 style={{ color: '#f00' }}>{this.state.counter}</h2>
         <Button onClick={this.handleClick}> Send </Button>
       </Fragment>
     );
   }
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById('app'));
